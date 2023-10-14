f3e673bffa24012274f3241fc4c0f1bfe74a6d16
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-10-15 00:45:11 +0300

    task1 done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,19 @@
+const hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"))
+const toolTip = document.createElement("div")
+toolTip.classList.add("tooltip")
+
+for (let item of hasTooltip) {
+    item.addEventListener("click", (event) => {
+        // find position to give it to our toolTip, after click
+        let position = item.getBoundingClientRect()
+
+        event.preventDefault()
+        item.insertAdjacentElement("afterend", toolTip)
+        let newText = item.getAttribute("title")
+        toolTip.textContent = newText
+        toolTip.classList.toggle("tooltip_active")
+
+        toolTip.style.top = position.bottom + 'px';
+        toolTip.style.left = position.left + 'px';
+    })
+}
